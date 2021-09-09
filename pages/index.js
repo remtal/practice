afffa4408881b093f4e329d7d93b566fda8d6c6f
remtal <tsamin99@gmail.com>
2021-09-09 11:22:07 +0100

    axios configured

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+import Axios from "axios";
 import Card from "../components/organisms/Card";
 import Greeting from "../components/molecules/Greeting";
-import React from "react";
 
 export default function Home() {
   const name = "Talha",
     surname = "Samin";
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://dummy-api.deta.dev/user").then((resp) => {
+      setData(resp.data);
+    });
+  }, []);
+
   return (
     <div>
-      <Card name={name} surname={surname} />
+      {/* <Card name={name} surname={surname} /> */}
+      {JSON.stringify(data)}
     </div>
   );
 }
