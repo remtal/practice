2da67d05286683b683f4d6879ba918f098ed9460
remtal <tsamin99@gmail.com>
2021-09-28 09:33:15 +0100

    Greeting retrieve from POST endpoint

--- components/molecules/Greeting/Greeting.tsx
+++ components/molecules/Greeting/Greeting.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+
+import Axios from "axios";
 import { GreetingStyle } from "./styles";
-import React from "react";
 
 interface Props {
   name: string;
 }
+
 export default function Greeting({ name }: Props) {
-  return <GreetingStyle>Hello {name}!</GreetingStyle>;
+  const [greeting, setGreeting] = useState([]);
+
+  Axios.post("https://dummy-api.deta.dev/greeting", {
+    name: name,
+  }).then((response) => {
+    setGreeting(response.data);
+  });
+
+  return <GreetingStyle>{greeting}</GreetingStyle>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,1 @@
-export { default } from "../components/pages/Home/Home";
+export { default } from "../components/pages/Home";
